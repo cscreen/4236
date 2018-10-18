c5a7f223b2687674f9dbe2b8772ca5678c4c8874
Christian Screen <cscreen@uncc.edu>
2018-10-18 12:43:35 -0400

    water & footsteps
    title

---
+++ Assets/Scripts/Sound_Script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Script : MonoBehaviour {
+
+    public AudioClip grunt;
+    public AudioSource gruntSource;
+
+    public AudioClip footStep;
+    public AudioSource footStepSource;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void gruntSound()
+    {
+        gruntSource.PlayOneShot(grunt);
+    }
+
+    void footStepSound()
+    {
+        footStepSource.PlayOneShot(footStep);
+    }
+}
